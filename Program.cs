0a5fd0028137ddbac199478abfcf66de0429d5f1
Adam Jacques <adam@noreply.git.technowizardry.net>
2026-05-19 18:39:41 -0700

    initial code

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+public class Config
+{
+}
+
+[YamlStaticContext]
+[YamlSerializable(typeof(Config))]
+partial class YamlStaticContext;
+
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        // Read and parse config.yml
+        var configYaml = await File.ReadAllTextAsync("config.yml");
+
+        var deserializer = new StaticDeserializerBuilder(new YamlStaticContext())
+            .WithNamingConvention(UnderscoredNamingConvention.Instance)
+            .Build();
+        var config = deserializer.Deserialize<Config>(configYaml);
+
+        Console.WriteLine(config);
+    }
+}
